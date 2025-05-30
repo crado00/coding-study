fb5881f2dc94be3ed136364186b93506a2d216ec
crado00 <alrlawowns@gmail.com>
2025-05-30 10:58:00 +0900

    double for loop

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("v2");
 
+        for(int i = 1; i <= 5; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
